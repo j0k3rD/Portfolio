851c1a05d2e4b7c6888885f51aabe125f461d971
j0k3r-Desktop <agm.moya@alumno.um.edu.ar>
2023-01-13 16:44:08 -0300

    fixing movil screen

--- pages/index.js
+++ pages/index.js
@@ -87,19 +87,19 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {/* ----------- MAIN BAR ----------- */}
-      <main className='bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900'>
-        <section className='min-h-screen'>
-          <nav className='pt-10 flex justify-between'>
-            <h1 className='text-xl font-burtons'>developedbyaaron</h1>
+      <main className='bg-white px-10 sm:px-1 md:px-20 lg:px-40 dark:bg-gray-900'>
+        <section className='sect'>
+          <nav className='nav-bar pt-10 flex justify-between'>
+            <h1 className='text-ini text-xl font-burtons'>developedbyaaron</h1>
             <ul className='flex items-center'>
               <li>
                 <BsFillMoonStarsFill 
                   onClick={() => setDarkMode(!darkMode)}
-                  className='cursor-pointer text-2xl'/>
+                  className='luna cursor-pointer text-2xl'/>
               </li>
               <li>
                 <a 
-                  className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' 
+                  className='btn-d bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' 
                   href="#">
                     Download CV
                 </a>
@@ -109,9 +109,9 @@ export default function Home() {
           {/* ----------- MAIN BAR ----------- */}
 
           {/* ----------- MENU ----------- */}
-          <div className='relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 mb-10 overflow-hidden md:h-96 md:w-96'>
+          <div className='ima relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 mb-10 overflow-hidden md:h-96 md:w-96 sm:w-0'>
               <Image src={deved} layout="fill" objectFit='cover' />
-            </div>
+          </div>
           <div className='text-center'>
             <h2 className='text-5xl py-2 text-teal-600 font-medium md:text-6xl'>
               Hi. I'm Aaron 🖐️
@@ -124,7 +124,7 @@ export default function Home() {
           <div>
             <div className='text-4xl text-center'>
               <h3 className='mb-7 mt-7 font-black'>My skills</h3>
-              <div className='text-6xl flex justify-center gap-16 p-3'>
+              <div className='text-6xl flex justify-center gap-16 p-3 md:grid sm:grid lg:flex'>
                 <div className=' text-blue-600'>
                   <DiPython /> 
                 </div>
@@ -166,7 +166,7 @@ export default function Home() {
               I always try to <span className=' text-teal-500'>make clean code</span> complying with design patterns and some quality attributes.
             </p>*/}
           </div>
-          <div className='lg:flex gap-10'>
+          <div className='md:flex-grid lg:flex gap-10'>
             <div className='text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white'>
               {/* <Image src={design} width={100} height={100} /> */}
               <h3 className='text-lg font-medium pb-2'>Fully Responsible 💼</h3>
