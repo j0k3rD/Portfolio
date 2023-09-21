e2bd8feea0207ee31167a702492f3a49ef9c37bc
Aaron Moya <agm.moya@alumno.um.edu.ar>
2023-09-21 17:02:43 -0300

    new portfolio 2023, material ui - react

---
+++ src/data/Constants.js
@@ -0,0 +1,361 @@
+export const Bio = {
+    name: "Aaron Moya",
+    roles: [
+      "Inf Eng Student",
+      "Programmer",
+      "Linux Enthusiast",
+    ],
+    description:
+      "My name is Aaron Moya, i'm a informatics engineering student at the University of Mendoza. I'm constantly learning new things. I like to read about programming and hacking.",
+    github: "https://github.com/j0k3rD",
+    resume:
+      "",
+    linkedin: "https://www.linkedin.com",
+    twitter: "https://twitter.com/aaron_moya22",
+    insta: "https://www.instagram.com/aaron_moya22/",
+  };
+  
+  export const skills = [
+    {
+      title: "Languages",
+      skills: [
+        {
+            name: "Python",
+            image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        },
+        {
+            name: "Java",
+            image:
+              "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+        },
+        {
+            name: "JavaScript",
+            image:
+              "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+            name: "HTML",
+            image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+            name: "CSS",
+            image:
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+      ],
+    },
+    {
+      title: "Frameworks | Databases",
+      skills: [
+
+        {
+            name: "Flask",
+            image: 'https://www.vectorlogo.zone/logos/pocoo_flask/pocoo_flask-icon.svg'
+        },
+        {
+            name: "Django",
+            image: 'https://www.vectorlogo.zone/logos/djangoproject/djangoproject-icon.svg'
+        },
+        {
+            name: "Spring Boot",
+            image: 'https://www.vectorlogo.zone/logos/springio/springio-icon.svg'
+        },
+        {
+            name: "Bootstrap",
+            image:
+              "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+        },
+        {
+            name: "MySQL",
+            image: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+        },
+        {
+            name: "Redis",
+            image: "https://www.vectorlogo.zone/logos/redis/redis-icon.svg",
+        }
+      ],
+    },
+    {
+      title: "Others",
+      skills: [
+        {
+            name: "Git",
+            image:
+                "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+        },
+        {
+            name: "GitHub",
+            image:
+                "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+        {
+            name: "Docker",
+            image:
+                "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+        },
+        {
+            name: "Android Studio",
+            image:
+                "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+        },
+        {
+            name: "Node Js",
+            image: "https://nodejs.org/static/images/logo.svg",
+        },
+        {
+            name: "React Js",
+            image:
+                "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        {
+            name: "VS Code",
+            image:
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+        {
+            name: "Postman",
+            image:
+                "https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667",
+        },
+        {
+            name: "Figma",
+            image:
+                "https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667",
+        },
+      ],
+    },
+  ];
+  
+  export const experiences = [
+
+  ];
+  
+  export const education = [
+    // {
+    //   id: 0,
+    //   img: "https://imgs.search.brave.com/xMvntwC2YscnRlKay1Ib6JUjF3wi_GYXaEXjLe-JHEE/rs:fit:860:0:0/g:ce/aHR0cHM6Ly93d3cu/bWFsYXJndWVhZGlh/cmlvLmNvbS93cC1j/b250ZW50L3VwbG9h/ZHMvMjAxNC8wNS9F/U1RJTS1ERS1PUk9f/VjUxLmpwZw",
+    //   school: "Escuela Técnica Química Industrial y Minera N° 4-018 Manuel Nicolás Savio.",
+    //   date: "2014 - 2019",
+    //   grade: "8.5/10",
+    //   desc: "I completed my high school education at Escuela Técnica Química Industrial y Minera N° 4-018 Manuel Nicolás Savio, where I studied for 6 years and obtained the title of Process Industry Technician.",
+    //   degree: "Process Industry Technician",
+    // },
+    // {
+    {
+      id: 2,
+      img: "https://um.edu.ar/wp-content/themes/um/img/logo--header.png",
+      school: "Universidad de Mendoza",
+      date: "2020 - 2024",
+    //   grade: "92.3%",
+      desc: "I am currently studying Informatics Engineering at the Universidad de Mendoza.",
+      degree: "Informatics Engineer",
+    },
+  ];
+  
+  export const projects = [
+        {
+        id: 0,
+        title: "ChatNet Hub",
+        date: "May 2023 - Present",
+        description:
+            "Client-server application, which simulates a chat in real time, where users communicate in different already armed groups. It uses django and microservices to run user login and real-time chat.",
+        image:
+            "https://user-images.githubusercontent.com/83615373/243244606-f4528688-35b4-404a-bff3-5a271f48ae47.png",
+        tags: [
+            "Python",
+            "Django",
+            "MySQL",
+            "Redis",
+            "Docker",
+            "CI/CD",
+            "React",
+        ],
+        category: "web app",
+        github: "https://github.com/j0k3rD/real_time_chat_project",
+        //   webapp: "",
+        member: [
+            {
+                name: "Aaron Moya",
+                img: "https://avatars.githubusercontent.com/u/83615514?v=4",
+                github: "https://github.com/j0k3rD",
+            },
+            {
+                name: "Nicolas Mayoral",
+                img: "https://avatars.githubusercontent.com/u/83615373?v=4",
+                github: "https://github.com/NKAmazing",
+            },
+            {
+                name: "Marcos Miglierina",
+                img: "https://avatars.githubusercontent.com/u/48894558?v=4",
+                github: "https://github.com/XxRaXoRxX",
+            },
+        ],
+    },
+    {
+        id: 1,
+        title: "PlatformUM (Backend)",
+        date: "May 2023 - Present",
+        description:
+          "",
+        image:
+          "",
+        tags: [
+            "Java",
+            "Spring Boot",
+            "Maven",
+            "MySQL",
+            "Hibernate"
+        ],
+        category: "android app",
+        github: "https://github.com/NKAmazing/PlatformUM",
+        member: [
+            {
+                name: "Aaron Moya",
+                img: "https://avatars.githubusercontent.com/u/83615514?v=4",
+                github: "https://github.com/j0k3rD",
+            },
+            {
+                name: "Nicolas Mayoral",
+                img: "https://avatars.githubusercontent.com/u/83615373?v=4",
+                github: "https://github.com/NKAmazing",
+            },
+            {
+                name: "Marcos Miglierina",
+                img: "https://avatars.githubusercontent.com/u/48894558?v=4",
+                github: "https://github.com/XxRaXoRxX",
+            },
+        ],
+    },
+    {
+        id: 2,
+        title: "PlatformUM (Frontend)",
+        date: "Jul 2023 - Present",
+        description:
+          "",
+        image:
+          "",
+        tags: [
+            "JavaScript",
+            "React Native",
+            "Expo"
+        ],
+        category: "android app",
+        github: "https://github.com/NKAmazing/PlatformUM_Frontend",
+        member: [
+            {
+                name: "Aaron Moya",
+                img: "https://avatars.githubusercontent.com/u/83615514?v=4",
+                github: "https://github.com/j0k3rD",
+            },
+            {
+                name: "Nicolas Mayoral",
+                img: "https://avatars.githubusercontent.com/u/83615373?v=4",
+                github: "https://github.com/NKAmazing",
+            },
+            {
+                name: "Marcos Miglierina",
+                img: "https://avatars.githubusercontent.com/u/48894558?v=4",
+                github: "https://github.com/XxRaXoRxX",
+            },
+        ],
+    },
+    {
+        id: 3,
+        title: "Distributed Port Scanner",
+        date: "Nov 2022 - Feb 2023",
+        description:
+          "A distributed port scanner made with Python and Redis. It uses Redis to store the results of the scan and Celery to distribute the tasks.",
+        image:
+          "https://user-images.githubusercontent.com/83615514/220962856-9a6286e1-5206-4dc9-9ab6-f5183a79b48a.png",
+        tags: [   
+                "Python",
+                "Asyncio",
+                "Django",
+                "Redis",
+                "MySQL",
+                "Celery",
+                "WebSockets"
+              ],
+        category: "web app",
+        github: "https://github.com/j0k3rD/distributed_port_scanner",
+      },
+    {
+      id: 4,
+      title: "CheatopIA",
+      date: "Jun 2023 - Jul 2023",
+      description:
+        "A navegator extension that helps you to find the best cheats. It uses ChatGPT to generate the cheats and shows them at same page you are.",
+      image:
+        "",
+      tags: [   
+                "JavaScript", 
+                "OpenAI",
+                "ChatGPT",
+                "Chrome Extension"
+            ],
+      category: "tools",
+      github: "https://github.com/j0k3rD/cheatopia",
+    },
+    {
+        id: 5,
+        title: "BotCoursesUM",
+        date: "Aug 2022 - Dic 2022",
+        description:
+            "Preprocessed and augmented the dataset to improve model accuracy, trained the model, created API using model and Python, and used React web app for the project's front end. Achievements: Achieved an accuracy of 99.2% to accurately detect brain tumors from medical images.",
+        image:
+            "https://cdn-icons-png.flaticon.com/512/6008/6008363.png",
+        tags: [   
+            "Python",
+            "Flask"
+        ],
+        category: "tools",
+        github: "https://github.com/j0k3rD/Bot_Courses_UM",
+        member: [
+            {
+                name: "Aaron Moya",
+                img: "https://avatars.githubusercontent.com/u/83615514?v=4",
+                github: "https://github.com/j0k3rD",
+            },
+            {
+                name: "Marcos Miglierina",
+                img: "https://avatars.githubusercontent.com/u/48894558?v=4",
+                github: "https://github.com/XxRaXoRxX",
+            },
+        ],
+    },
+    {
+      id: 6,
+      title: "Power Outage App",
+      date: "May 2023",
+      description:
+        "A android app that shows the power outages in the city of Mendoza. It uses the Telegram Bot API to send the notifications.",
+      image:
+        "",
+    tags: [   
+        "Java",
+        "Android Studio",
+        "Telegram Bot API"
+    ],
+      category: "android app",
+      github: "https://github.com/j0k3rD/power_outage_app",
+    },
+    {
+        id: 7,
+        title: "The Poetizer",
+        date: "Apr 2022 - Nov 2022",
+        description:
+            "A web app for poets. It allows you to create poems and share them with the community.",
+        image:
+            "https://user-images.githubusercontent.com/83615514/202943480-72f8dadd-8d37-468e-a9bf-16638dbb4410.png",
+        tags: [   
+            "Python",
+            "Flask",
+            "SQLite",
+            "Bootstrap",
+            "HTML",
+            "CSS"
+        ],
+        category: "web app",
+        github: "https://github.com/j0k3rD/the_poetizer_web",
+    }
+  ];
